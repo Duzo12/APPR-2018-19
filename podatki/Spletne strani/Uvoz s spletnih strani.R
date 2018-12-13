85303f87d1d378fe378d8e4ebc92deaf72e32636
Nejc Duscak <nejc.duscak@hotmail.com>
2018-12-13 09:47:53 +0100

    Rezanje html

--- podatki/Spletne strani/Uvoz s spletnih strani.R
+++ podatki/Spletne strani/Uvoz s spletnih strani.R
@@ -2,6 +2,7 @@ library(rvest)
 library(gsubfn)
 library(readr)
 library(dplyr)
+library(XML)
 
 link <- "https://en.wikipedia.org/wiki/List_of_countries_by_median_age"
 stran <- html_session(link) %>% read_html()
@@ -17,4 +18,9 @@ link <- "https://www.worldatlas.com/articles/murder-rates-by-country.html"
 stran <- html_session(link) %>% read_html()
 tabelaUmorov <- stran %>% html_nodes(xpath = "//table") %>%
   .[[1]] %>% html_table(dec= ",")
+
+#Poskus s spletne csv
+link <- "https://www.numbeo.com/cost-of-living/country_price_rankings?displayCurrency=USD&itemId=105"
+stran <- html_session(link) %>% read_html() %>% as.character()
+stran1 <- gsub("^\\([\n(.*)]\\);$", "\\1", stran)
 
