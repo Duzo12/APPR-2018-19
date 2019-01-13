2e2840a27265b0d22628c2a6e9a4666f7587f127
Nejc Duščak <nejc.duscak@hotmail.com>
2019-01-13 22:24:57 +0100

    Malo grafov in analize

---
+++ podatki/Grafi/Grafi.R
@@ -0,0 +1,31 @@
+require(ggplot2)
+require(dplyr)
+
+ggplot(data =SkupnaTabela, aes(x=Drzava, y=Vrednost.BDP)) +
+  geom_bar(stat = "Identity") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1),text = element_text(size=5)) +
+  coord_flip()
+
+ggplot(SkupnaTabela, aes(x = Visina.place, y = Vrednost.BDP)) +
+  geom_point() +
+  geom_smooth(method = lm, se= F)
+
+ggplot(SkupnaTabela, aes(x = Visina.place, y = Povprecje.let)) +
+  geom_point() +
+  geom_smooth(method =  lm)
+
+ggplot(SkupnaTabela, aes(x = Visina.place, y = Indeks.izobrazbe)) +
+  geom_point() +
+  geom_smooth(method =  lm)
+
+ggplot(SkupnaTabela, aes(x = Visina.place, y = Stopnja.kriminala)) +
+  geom_point() +
+  geom_smooth(method =  lm)
+
+ggplot(SkupnaTabela, aes(x = Visina.place, y = Zivljenjski.stroski)) +
+  geom_point() +
+  geom_smooth(method =  lm)
+
+ggplot(SkupnaTabela, aes(x = Indeks.izobrazbe , y = Zivljenjski.stroski )) +
+  geom_point() +
+  geom_smooth(method =  lm)
