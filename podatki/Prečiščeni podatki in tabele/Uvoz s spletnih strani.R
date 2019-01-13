4db2f580a86a926968961d7d8ea29f0621dac6b2
Nejc Duščak <nejc.duscak@hotmail.com>
2019-01-13 22:00:38 +0100

    Uredil tabele

--- podatki/Prečiščeni podatki in tabele/Uvoz s spletnih strani.R
+++ podatki/Prečiščeni podatki in tabele/Uvoz s spletnih strani.R
@@ -8,20 +8,24 @@ library(dplyr)
 library(XML)
 library(openxlsx)
 
-tabelaIzobrazba <- read.xlsx("U:/_Osebno/Projekt APPR/APPR-2018-19/Projekt APPR/podatki/Izobrazba.xlsx")
+#"U:/_Osebno/Projekt APPR/APPR-2018-19/Projekt APPR/podatki/Izobrazba.xlsx"
+tabelaIzobrazba <- read.xlsx("C:/Users/nejc/Desktop/FMF/Program R/ProjektAPPR/Novi projekt/ProjektAPPR/podatki/Izobrazba.xlsx")
 obdrzistolpec <- c("Country", "2017")
 tabelaIzobrazba <- tabelaIzobrazba[ , obdrzistolpec]
 names(tabelaIzobrazba) <- c("Drzava", "Indeks.izobrazbe")
 tabelaIzobrazba$Indeks.izobrazbe <- as.numeric(as.character(tabelaIzobrazba$Indeks.izobrazbe))
 tabelaIzobrazba$Indeks.izobrazbe <- tabelaIzobrazba$Indeks.izobrazbe * 100
+tabelaIzobrazba$Drzava <- gsub("^[[:space:]]*", "", tabelaIzobrazba$Drzava)
 
 
 linkStarosti <- "https://en.wikipedia.org/wiki/List_of_countries_by_median_age"
 stranStarosti <- html_session(linkStarosti) %>% read_html()
 tabelaStarosti <- stranStarosti %>% html_nodes(xpath = "//table[@class='wikitable sortable']") %>%
   .[[1]] %>% html_table(dec= ",")
-tabelaStarosti <- tabelaStarosti[ , -2]
-names(tabelaStarosti) <- c("Drzava", "Povprecje.let", "Povprecje.moskih.let", "Povprecje.zenskih.let")
+obrdzi <- c("Country/Territory", "Total(Year)")
+tabelaStarosti <- tabelaStarosti[ , obrdzi]
+names(tabelaStarosti) <- c("Drzava", "Povprecje.let")
+
 
 linkBDP <- "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita#Distorted_GDP-per-capita_for_tax_havens"
 stranBDP <- html_session(linkBDP) %>% read_html()
@@ -61,10 +65,12 @@ drzave <- gsub("^.*'(.*)'.*$", "\\1",stran1)
 cifre=gsub("^.*, (.*)\\].*$", "\\1",stran1)
 tabelaCostliving = data.frame(Drzave=drzave,vrednost=as.numeric(cifre))[seq(3,length(cifre)-2,2),]
 names(tabelaCostliving) <- c("Drzava", "Zivljenjski.stroski")
+tabelaCostliving$Drzava <- as.character(as.factor(tabelaCostliving$Drzava))
 
 #Tabela, ki združuje vse tabele
 Skupnatabela <- tabelaPlace %>% inner_join(tabelaBDP, "Drzava"="Drzava") %>%
-  inner_join(tabelaStarosti, "Drzava"="Drzava") %>%
-  inner_join(tabelaCrimeindex, "Drzava"="Drzava") %>%
-  inner_join(tabelaCostliving, "Drzava"="Drzava")  
+                inner_join(tabelaStarosti, "Drzava"="Drzava") %>%
+                inner_join(tabelaIzobrazba, "Drzava" = "Drzava") %>%
+                inner_join(tabelaCrimeindex, "Drzava"="Drzava") %>%
+                inner_join(tabelaCostliving, "Drzava"="Drzava")
   
