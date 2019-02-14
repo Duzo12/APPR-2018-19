4d7be5cf30d11d44c2d0957585cf3b9acfab643a
Nejc Duščak <nejc.duscak@hotmail.com>
2019-02-14 11:20:31 +0100

    Uredil grafe

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -8,10 +8,10 @@ ggplot(data =SkupnaTabela, aes(x=reorder(Drzava, -Vrednost.BDP), y=Vrednost.BDP)
   theme(axis.text.x = element_text(angle = 90, hjust = 1),text = element_text(size=5)) +
   coord_flip()
 
-ggplot(data=SkupnaTabela, aes(x=reorder(Drzava, -Visina.place), y=Visina.place)) + 
-  geom_bar(stat = "Identity") +
+ggplot(data=SkupnaTabela, aes(x=Visina.place, y=reorder(Drzava, -Visina.place), fill=factor(ifelse(area="Switzerland", "Highlighted","Normal" )))) + 
+  geom_bar(stat = "Identity") + scale_fill_manual(name = "area", values = c("red", "grey50")) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1),text = element_text(size=5)) +
-  coord_flip()
+  coord_flip() 
 
 
 #Iskal povezave med grafi
@@ -22,20 +22,24 @@ ggplot(SkupnaTabela, aes(x = Visina.place, y = Vrednost.BDP)) +
 
 ggplot(SkupnaTabela, aes(x = Visina.place, y = Povprecje.let)) +
   geom_point() +
-  geom_smooth(method =  lm)
+  geom_smooth(method = loess)
 
 ggplot(SkupnaTabela, aes(x = Visina.place, y = Indeks.izobrazbe)) +
   geom_point() +
-  geom_smooth(method =  lm)
+  geom_smooth(method =  loess)
 
 ggplot(SkupnaTabela, aes(x = Visina.place, y = Stopnja.kriminala)) +
   geom_point() +
-  geom_smooth(method =  lm)
+  geom_smooth(method =  loess)
 
 ggplot(SkupnaTabela, aes(x = Visina.place, y = Zivljenjski.stroski)) +
   geom_point() +
-  geom_smooth(method =  lm)
+  geom_smooth(method =  loess)
 
 ggplot(SkupnaTabela, aes(x = Indeks.izobrazbe , y = Zivljenjski.stroski )) +
   geom_point() +
-  geom_smooth(method =  lm)
+  geom_smooth(method =  loess)
+
+ggplot(SkupnaTabela, aes(x = Indeks.izobrazbe, y = Povprecje.let )) +
+  geom_point() +
+  geom_smooth(method = loess)
