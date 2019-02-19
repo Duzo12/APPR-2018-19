ae1e06a46ccc3516e94604d40e1fec3cc1be33a0
Nejc Duščak <nejc.duscak@hotmail.com>
2019-02-19 10:35:36 +0100

    shinyy

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,25 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  titlePanel("Analiza neto plač po svetu"),
+    tabPanel("Tabela starosti",
+               DT::dataTableOutput("tabela")),
   
-  titlePanel("Slovenske občine"),
-  
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
+    tabPanel("Osnovni podatki o programu",
+      sidebarLayout(
+        mainPanel(
+          h1("Uvod"),
+          h5("jahrfahrio"))
+      )
+
     )
-))
+  
+
+  
+    #,  tabPanel("Število naselij",
+               #sidebarPanel(
+                  #uiOutput("pokrajine")
+                #),
+               #mainPanel(plotOutput("naselja")))
+    )
+)
